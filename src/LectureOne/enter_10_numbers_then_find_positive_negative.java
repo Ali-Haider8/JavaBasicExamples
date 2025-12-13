9536c8186e2b7d72de5d3e74426ee5877ad498af
Ali Nukhailawi <26713386+Ali-Haider8@users.noreply.github.com>
2025-12-13 17:32:37 +0300

    initial commit

---
+++ src/LectureOne/enter_10_numbers_then_find_positive_negative.java
@@ -0,0 +1,32 @@
+package LectureOne;
+
+import java.util.Scanner;
+
+public class enter_10_numbers_then_find_positive_negative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+
+        System.out.println("Enter 10 numbers: ");
+        for (int i = 1; i < 10; i++) {
+            numbers[i] = input.nextInt();
+        }
+
+        System.out.println("Positive Numbers: ");
+        for (int i = 1; i < 10; i++) {
+            if (numbers[i] > 0)
+                System.out.println(numbers[i] + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("Negative Numbers: ");
+        for (int i = 0; i < 10; i++) {
+            if (numbers[i] < 0)
+                System.out.println(numbers[i] + " ");
+        }
+
+        input.close();
+    }
+}
---
+++ src/LectureOne/positive_and_negative_10_num.java
@@ -0,0 +1,24 @@
+package LectureOne;
+
+import java.util.Scanner;
+
+public class positive_and_negative_10_num {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Enter an integer number: ");
+            n = input.nextInt();
+            if (n == 0)
+                System.out.println("Invalid input");
+            else if (n > 0)
+                System.out.println(n + " is positive");
+            else
+                System.out.println(n + " is negative");
+
+        }
+
+    }
+
+}
