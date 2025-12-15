64ccc77ad8e16b0d653c9d436fe5b0c5b4f18860
Ali Nukhailawi <26713386+Ali-Haider8@users.noreply.github.com>
2025-12-15 23:26:00 +0300

    initial commit

---
+++ src/LectureOne/positive_and_negative_between_1_to_10.java
@@ -0,0 +1,16 @@
+package LectureOne;
+
+import java.util.Scanner;
+
+public class positive_and_negative_between_1_to_10 {
+    public static void main(String[] args) {
+        System.out.println("Even and odd numbers between 1 and 10:");
+
+        for (int i = 1; i <= 10; i++)
+            if (i % 2 == 0)
+                System.out.println(i + " is even");
+            else
+                System.out.println(i + " is odd");
+
+    }
+}
