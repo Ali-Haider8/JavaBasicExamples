2129d9d4f976f0576e283f4b96da29940dcb5108
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-01-10 05:10:43 +0300

    update

--- src/Homeworks/AverageResult.java
+++ src/Homeworks/AverageResult.java
@@ -12,7 +12,6 @@ public class AverageResult {
             if (value.equalsIgnoreCase("done"))
                 return Double.NaN;
 
-
             try {
                 double mark = Double.parseDouble(value);
 
@@ -59,13 +58,22 @@ public class AverageResult {
             count++;
         }
 
-        double avg = sum / count;
+        int avg = (int) (sum / count);
 
+        System.out.println();
         System.out.println("Average: " + avg);
+
+        if (avg >= 45 && avg < 50) {
+            int carveMarks = 50 - avg;
+            System.out.println("You got a carve: " + carveMarks);
+            avg = avg + carveMarks;
+            System.out.println("Your Average After Carve : " + avg);
+        }
+
         if (avg >= 50)
-            System.out.println("Passed");
+            System.out.println("Result: " + "Passed");
         else
-            System.out.println("Failed");
+            System.out.println("Result: " + "Failed");
 
         sc.close();
 
